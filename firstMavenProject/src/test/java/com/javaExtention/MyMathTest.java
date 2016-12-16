083ea02eb4aba1091209498ff33094471748bcc7
Alexander Tsaregorodtsev <czar_ijb@mail.ru>
2016-12-16 14:17:22 +0700

    import org.junit.Test annotation is working

--- firstMavenProject/src/test/java/com/javaExtention/MyMathTest.java
+++ firstMavenProject/src/test/java/com/javaExtention/MyMathTest.java
@@ -1,12 +1,13 @@
 package com.javaExtention;
 
-import junit.framework.Test;
+
 import junit.framework.TestCase;
+import org.junit.Test;
 
 /**
  * Created by 1 on 10-Dec-16.
  */
-public class MyMathTest extends TestCase{
+public class MyMathTest extends TestCase {
     private MyMath mm = new MyMath();
 
     @Test
